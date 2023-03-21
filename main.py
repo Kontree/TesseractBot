efe0d19b7ec4e004e8b58848cabb402b34f10a16
Konstantin Boboshko <kostyaboboshko123@gmail.com>
2023-03-21 16:09:51 +0300

    modified bot to handle every language that can be processed by tesseract

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(content_types=['photo'])
 def handle(message):
+    language = message.__dict__['caption'] if message.__dict__['caption'] else 'eng'
     photo = message.photo[-1]
     image_in_web = bot.get_file(photo.file_id)
     image_file = requests.get(f'https://api.telegram.org/file/bot{BOT_TOKEN}/{image_in_web.file_path}')
     with open(f'image.jpg', 'wb') as image:
         image.write(image_file.content)
     image = cv2.imread('image.jpg')
-    string = pytesseract.image_to_string(image)
+    try:
+        string = pytesseract.image_to_string(image, lang=language)
+    except pytesseract.pytesseract.TesseractError:
+        string = f'Couldn\'t recognize your language. Try again with different language.'
     bot.reply_to(message, string)
 
 
